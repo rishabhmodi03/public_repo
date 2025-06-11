3b3cf7fc12d6fb3c3eaa4761384dc13a5e315c23
Rishabh Modi <88582334+rishabhmodi03@users.noreply.github.com>
2025-06-11 13:58:22 +0530

    Update remote_feature.py

--- remote_feature.py
+++ remote_feature.py
@@ -5,7 +5,7 @@ from utils_for_lambda import send_followup_message
 def my_slash_command(interaction_data):
     token = interaction_data["token"]
     app_id = interaction_data["application_id"]
-    send_followup_message(app_id, token, content="Follow-up from remote!", ephemeral=True)
+    send_followup_message(token, content="Follow-up from remote!", ephemeral=True)
     return generate_response(content="Hello from remote slash!", ephemeral=True)
 
 def my_button_handler(interaction_data):
